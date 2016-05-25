706982094d0767e2723925b947a67a10ecdcb1ba
caledfwlch1 <caledfwlch1@gmail.com>
2016-05-25 16:13:27 +0300

    new test

--- temp.go
+++ temp.go
@@ -2,217 +2,65 @@ package main
 
 import (
 	"fmt"
-	"encoding/binary"
-	"bytes"
-	"io"
-//	"bufio"
-	"strings"
 )
 
-const (
-	LenPacket 		= 2048
+type Iner interface {
+	Set(n string, i int)
+}
+
+type T1 struct {
+	Name string
+	Value int
+	Inter Iner
+}
+
+type T2 struct {
+	Name string
+	Value int
+	Inter Iner
+}
+
+var (
+	_ Iner = (*T1)(nil)
+	_ Iner = (*T2)(nil)
 )
 
-func main() { // Den: Лучше это сделать тестом.
-	var h ISCSIConnection
 
-	fmt.Println("start")
-	//login packet
+func (p *T1)Set(n string, i int){
+	p.Name = n
+	p.Value = i
+}
 
-	pack := []byte{
-		0x43, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf7, /* C....... */
-		0x00, 0x02, 0x3d, 0x00, 0x00, 0x00, 0x00, 0x00, /* ..=..... */
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* ........ */
-		0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, /* ........ */
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* ........ */
-		0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, /* ........ */
-		0x49, 0x6e, 0x69, 0x74, 0x69, 0x61, 0x74, 0x6f, /* Initiato */
-		0x72, 0x4e, 0x61, 0x6d, 0x65, 0x3d, 0x69, 0x71, /* rName=iq */
-		0x6e, 0x2e, 0x31, 0x39, 0x39, 0x33, 0x2d, 0x30, /* n.1993-0 */
-		0x38, 0x2e, 0x6f, 0x72, 0x67, 0x2e, 0x64, 0x65, /* 8.org.de */
-		0x62, 0x69, 0x61, 0x6e, 0x3a, 0x30, 0x31, 0x3a, /* bian:01: */
-		0x31, 0x38, 0x37, 0x61, 0x39, 0x31, 0x61, 0x66, /* 187a91af */
-		0x34, 0x30, 0x00, 0x49, 0x6e, 0x69, 0x74, 0x69, /* 40.Initi */
-		0x61, 0x74, 0x6f, 0x72, 0x41, 0x6c, 0x69, 0x61, /* atorAlia */
-		0x73, 0x3d, 0x73, 0x69, 0x74, 0x2d, 0x31, 0x39, /* s=sit-19 */
-		0x32, 0x30, 0x00, 0x53, 0x65, 0x73, 0x73, 0x69, /* 20.Sessi */
-		0x6f, 0x6e, 0x54, 0x79, 0x70, 0x65, 0x3d, 0x44, /* onType=D */
-		0x69, 0x73, 0x63, 0x6f, 0x76, 0x65, 0x72, 0x79, /* iscovery */
-		0x00, 0x48, 0x65, 0x61, 0x64, 0x65, 0x72, 0x44, /* .HeaderD */
-		0x69, 0x67, 0x65, 0x73, 0x74, 0x3d, 0x4e, 0x6f, /* igest=No */
-		0x6e, 0x65, 0x00, 0x44, 0x61, 0x74, 0x61, 0x44, /* ne.DataD */
-		0x69, 0x67, 0x65, 0x73, 0x74, 0x3d, 0x4e, 0x6f, /* igest=No */
-		0x6e, 0x65, 0x00, 0x44, 0x65, 0x66, 0x61, 0x75, /* ne.Defau */
-		0x6c, 0x74, 0x54, 0x69, 0x6d, 0x65, 0x32, 0x57, /* ltTime2W */
-		0x61, 0x69, 0x74, 0x3d, 0x32, 0x00, 0x44, 0x65, /* ait=2.De */
-		0x66, 0x61, 0x75, 0x6c, 0x74, 0x54, 0x69, 0x6d, /* faultTim */
-		0x65, 0x32, 0x52, 0x65, 0x74, 0x61, 0x69, 0x6e, /* e2Retain */
-		0x3d, 0x30, 0x00, 0x49, 0x46, 0x4d, 0x61, 0x72, /* =0.IFMar */
-		0x6b, 0x65, 0x72, 0x3d, 0x4e, 0x6f, 0x00, 0x4f, /* ker=No.O */
-		0x46, 0x4d, 0x61, 0x72, 0x6b, 0x65, 0x72, 0x3d, /* FMarker= */
-		0x4e, 0x6f, 0x00, 0x45, 0x72, 0x72, 0x6f, 0x72, /* No.Error */
-		0x52, 0x65, 0x63, 0x6f, 0x76, 0x65, 0x72, 0x79, /* Recovery */
-		0x4c, 0x65, 0x76, 0x65, 0x6c, 0x3d, 0x30, 0x00, /* Level=0. */
-		0x4d, 0x61, 0x78, 0x52, 0x65, 0x63, 0x76, 0x44, /* MaxRecvD */
-		0x61, 0x74, 0x61, 0x53, 0x65, 0x67, 0x6d, 0x65, /* ataSegme */
-		0x6e, 0x74, 0x4c, 0x65, 0x6e, 0x67, 0x74, 0x68, /* ntLength */
-		0x3d, 0x33, 0x32, 0x37, 0x36, 0x38, 0x00, 0x00} /* =32768.. */
+func (p *T2)Set(n string, i int){
+	p.Name = n
+	p.Value = i
+}
 
+func main() {
+	var t1 T1
+	var t2 T2
 
+	t1 = T1{Name:"aaa", Value:10}
+	t2 = T2{Name:"bbb", Value:20}
+	fmt.Println("begin", t1, t2)
+	//t1.Inter.Set("ccc", 30)
+	inter(t2.Inter)
+	fmt.Println("end", t1, t2)
+}
 
-	buf := bytes.NewReader(pack)
-	n, err := h.ReadFrom(buf)
-	fmt.Println("65 - n=", n, "\n65 - err=", err) // , "\n65 - h=", h)
+func inter(v interface{}) {
+/*
+	switch a := v.(type) {
+		case T1:
+			a.Set("T1", 30)
+			fmt.Println("Type T1 ", a)
+		case T2:
+			a.Set("T2", 30)
+			fmt.Println("Type T2 ", a)
+	}
+*/
+	//v.Set("ccc", 30)
 
-	bufw := bytes.NewBuffer(nil)
-	n, err = h.WriteTo(bufw)
-	fmt.Println("69 - n=", n, "\n69 - err=", err, "\n69 - buf=", bufw, "len(buf)=", bufw.Len())
-
+	v.Inter.Set("ccc", 30)
 	return
 }
-
-type LoginHeader struct {
-	OpCode		byte		// 1 byte
-	OpCodeSF	byte		// 1 byte OpCodeSpcField
-	I		bool		// Immediate bit
-	T		bool		// Transit bit
-	C		bool		// Continue bit
-	CSG		int		// Current stage
-	NSG		int		// Next stage
-	VerMax		byte		// 1 byte
-	VerMin		byte		// 1 byte
-	TotAHSLen	int		// 1 byte
-	DataSegLen	int		// 3 bytes
-	ISID		[6]byte		// 6 bytes
-	TSIH		[2]byte		// 2 bytes
-	InitTaskTag	[4]byte		// 4 bytes
-	CID		[2]byte		// 2 bytes
-	Res1		[2]byte		// 2 bytes
-	CmdSN		int		// 4 bytes
-	ExpStatSN	int		// 4 bytes
-	Res2		[16]byte	// 16 bytes
-	DataW		[]byte		//
-	msg		Msg
-}
-
-var _ Msg = (*LoginHeader)(nil)
-
-type ISCSIConnection struct {
-	LH	LoginHeader
-	DS	map[string]string	// Login parameters
-	DataR	[]byte			// Data read from ...
-	DataW	[]byte			// Data write to ...
-}
-
-type Msg interface {
-	ReadFrom(io.Reader) (int, error)
-	WriteTo(io.Writer) (int, error)
-//	Close(io.ReadWriteCloser) (error)
-}
-
-func (p *LoginHeader) ReadFrom(r io.Reader) (int, error) {
-	buf := make([]byte, 48)
-	n, err := io.ReadFull(r, buf)
-
-	p.I		= selectBit((buf[0]), 0x40)
-	p.OpCode 	= buf[0] & 0x3f
-	p.OpCodeSF	= buf[1]
-	p.T		= selectBit(p.OpCodeSF, 0x80)
-	p.C		= selectBit(p.OpCodeSF, 0x40)
-	p.CSG		= int(binary.BigEndian.Uint16([]byte{0, selectBits(p.OpCodeSF, 0x0c)}))
-	p.NSG		= int(binary.BigEndian.Uint16([]byte{0, selectBits(p.OpCodeSF, 0x03)}))
-	p.VerMax	= buf[2]
-	p.VerMin	= buf[3]
-	p.TotAHSLen	= int(binary.BigEndian.Uint16([]byte{0, buf[4]}))
-	p.DataSegLen	= int(binary.BigEndian.Uint32(append([]byte{0}, buf[5:8]...)))
-	_ = copy(p.ISID[:], buf[8:14])
-	_ = copy(p.TSIH[:], buf[14:16])
-	_ = copy(p.InitTaskTag[:], buf[16:20])
-	_ = copy(p.CID[:], buf[20:22])
-	_ = copy(p.Res1[:], buf[22:24])
-	_ = copy(p.Res2[:], buf[32:])
-	// p.ISID		= buf[8:14]
-	//p.TSIH		= buf[14:16]
-	//p.InitTaskTag	= buf[16:20]
-	//p.CID		= buf[20:22]
-	//p.Res1		= buf[22:24]
-	p.CmdSN		= int(binary.BigEndian.Uint32(buf[24:28]))
-	p.ExpStatSN	= int(binary.BigEndian.Uint32(buf[28:32]))
-	//p.Res2		= buf[32:]
-	return n, err
-}
-
-func (p *ISCSIConnection)ReadFrom(r io.Reader) (int, error) {
-
-	n, err := p.LH.ReadFrom(r)
-
-	buf := make([]byte, p.LH.DataSegLen)
-	n, err = io.ReadFull(r, buf)
-
-	p.Decode(buf)
-
-
-	return n, err
-}
-
-func (p *ISCSIConnection) Decode(d []byte) error {
-	p.DS = make(map[string]string, 0)
-	arrString := strings.Split(string(d), string("\x00"))
-	for _, element := range arrString {
-		if strings.Contains(element, "=") {
-			ar := strings.Split(element, "=")
-			p.DS[ar[0]] = ar[1]
-		}
-	}
-    return nil
-}
-
-func selectBit(bi, bc byte) (bool) {
-	return (bi & bc) == bc
-}
-
-func selectBits(bi, bc byte) (byte) {
-	return (bi & bc)
-}
-
-func (p LoginHeader)String() (string) {
-	return fmt.Sprintf("p.OpCode=%x, p.OpCodeSF=%x, p.I=%t, p.T=%t, p.C=%t, p.CSG=%d, p.NSG=%d, p.VerMax=%x, p.VerMin=%x, p.TotAHSLen=%d,"+
-			" p.DataSegLen=%d, p.ISID=%x, p.TSIH=%x, p.InitTaskTag=%x, p.CID=%x, p.Res1=%x, p.CmdSN=%d,"+
-			" p.ExpStatSN=%d, p.Res2=%x",
-			p.OpCode, p.OpCodeSF, p.I, p.T, p.C, p.CSG, p.NSG, p.VerMax, p.VerMin, p.TotAHSLen,
-			p.DataSegLen, p.ISID, p.TSIH, p.InitTaskTag, p.CID, p.Res1, p.CmdSN,
-			p.ExpStatSN, p.Res2)
-}
-
-func (p ISCSIConnection)String() (string) {
-	out := ""
-	for i, j := range p.DS {
-		out += " " + fmt.Sprint(i) + "=" + fmt.Sprint(j)
-	}
-	return fmt.Sprintf("LoginHeader=%s, \nParam=%s", p.LH, out)
-}
-
-func (p *ISCSIConnection)WriteTo(w io.Writer) (int, error) {
-	p.MakePacket()
-	n, err := w.Write(p.DataW)
-	return n, err
-}
-
-func (p *LoginHeader)WriteTo(w io.Writer) (int, error) {
-	n, err := w.Write(p.DataW)
-	return n, err
-}
-
-func (p *ISCSIConnection)MakePacket() {
-
-
-
-	p.DataW = []byte{0x43, 0x87, 0x00, 0x00, 0x00, 0x00, 0x00, 0xf7,
-			0x00, 0x02, 0x3d, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,
-			0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
-	return
-}
-
