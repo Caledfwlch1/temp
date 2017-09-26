1946c5a9f6f86ac65553605cf43736cb3be068ba
andy <caledfwlch1@gmail.com>
2017-09-26 13:26:13 +0300

    commit changed

--- runcurl/threshold_test.go
+++ runcurl/threshold_test.go
@@ -2,10 +2,12 @@ package runcurl
 
 import (
 	"testing"
+	"time"
 )
 
 func TestThreshold(t *testing.T) {
-	RunCurl("http://35.190.167.185/custom?branch=dev&commit=e43c8b290fb575808b8aafb680a45eeeb2221282&roles=threshold&wait=true")
-	//RunCurl("http://localhost/")
+	//RunCurl("http://35.190.167.185/custom?branch=dev&commit=e43c8b290fb575808b8aafb680a45eeeb2221282&roles=threshold&wait=true")
+	time.Sleep(time.Minute * 11)
+	RunCurl("http://localhost/")
 	return
 }
